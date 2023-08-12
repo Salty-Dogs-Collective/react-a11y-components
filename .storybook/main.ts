e7d3cf74ae258e29179c8f4594a582555ae89ef4
Patrick Söllner <patrick.soellner@aviv-group.com>
2023-08-12 14:01:47 +0200

    feat: add storybook styling with tailwind

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,15 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    {
+      name: '@storybook/addon-styling',
+      options: {},
+    },
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
