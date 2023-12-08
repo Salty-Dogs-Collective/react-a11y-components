c2aa722464110f8392e7eff1145cb7e683606eb5
Patrick Söllner <patrick.soellner@aviv-group.com>
2023-12-08 15:47:12 +0100

    feat: update deps

--- src/utils/class-names.ts
+++ src/utils/class-names.ts
@@ -1,21 +1,21 @@
 export const mergeOptionalClassNameWithList = (classNameList: string[], className?: string) => {
-    return (className ? [...className.split(' '), ...classNameList] : classNameList).join(' ');
+  return (className ? [...className.split(' '), ...classNameList] : classNameList).join(' ');
 };
 
 if (import.meta.vitest) {
-    const { it, describe } = import.meta.vitest;
+  const { it, describe } = import.meta.vitest;
 
-    describe.concurrent('class-names.ts', () => {
-        it('merge with one class', async ({ expect }) => {
-            expect(mergeOptionalClassNameWithList(['a'], 'b')).toEqual('b a');
-        });
-        
-        it('merge with two classes', async ({ expect }) => {
-            expect(mergeOptionalClassNameWithList(['a'], 'b c')).toEqual('b c a');
-        });
-
-        it('merge with no class', async ({ expect }) => {
-            expect(mergeOptionalClassNameWithList(['a'])).toEqual('a');
-        });
+  describe.concurrent('class-names.ts', () => {
+    it('merge with one class', async ({ expect }) => {
+      expect(mergeOptionalClassNameWithList(['a'], 'b')).toEqual('b a');
     });
+
+    it('merge with two classes', async ({ expect }) => {
+      expect(mergeOptionalClassNameWithList(['a'], 'b c')).toEqual('b c a');
+    });
+
+    it('merge with no class', async ({ expect }) => {
+      expect(mergeOptionalClassNameWithList(['a'])).toEqual('a');
+    });
+  });
 }
